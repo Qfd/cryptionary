b229e5394c035d49b82c95fd3fe28544592d55d5
Joshua Evans <joshua.evans@db.com>
2018-01-05 16:03:51 +0000

    add google analytics

--- src/components/head.js
+++ src/components/head.js
@@ -26,5 +26,9 @@ export default () => (
       rel="stylesheet"
     />
     <link href="/styles.css" rel="stylesheet" />
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112018770-1"/>
+    <script dangerouslySetInnerHTML={{ __html:`
+      window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-112018770-1');
+    `}}/>
   </head>
 );
